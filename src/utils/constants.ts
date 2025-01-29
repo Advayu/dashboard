749f92cfcadd0603c8352b3fafcccf726c561297
mriduljainn <mridul@advayu.club>
2025-01-30 00:27:09 +0530

    change the baseurl for production

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -31,9 +31,9 @@ export const paymentMethodsOptions = [
 export const URLREGEX =
   /^(https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/g;
 
-// export const LAMBDA_URL =
-//   "https://z5y61k807h.execute-api.us-east-1.amazonaws.com/dashboard";
-export const LAMBDA_URL = "http://localhost:4200";
+export const LAMBDA_URL =
+  "https://z5y61k807h.execute-api.us-east-1.amazonaws.com/dashboard";
+// export const LAMBDA_URL = "http://localhost:4200";
 
 export const AWS_IMAGE_UPLOAD_URL =
   "https://z5y61k807h.execute-api.us-east-1.amazonaws.com/dashboard/upload/image";
