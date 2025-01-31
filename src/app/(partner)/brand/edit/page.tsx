691d75eb3538204afe68d9ee98cf8d173fb71e02
mriduljainn <mridul@advayu.club>
2025-01-31 13:51:12 +0530

    fix overflow for small device

--- src/app/(partner)/brand/edit/page.tsx
+++ src/app/(partner)/brand/edit/page.tsx
@@ -284,7 +284,7 @@ const BrandDetails: React.FC<any> = () => {
   // advayu-onboarding-assets/outlet-assets
   return (
     <div className="w-full">
-      <div className="flex justify-between my-10 md:mx-8 mx-6 items-center">
+      <div className="flex justify-between my-10 md:mx-8 mx-5 items-center">
         <div className="flex items-center">
           <button
             className="flex items-center"
@@ -304,7 +304,7 @@ const BrandDetails: React.FC<any> = () => {
           <Loading />
         </div>
       ) : (
-        <div className="flex flex-col items-center justify-center w-full md:mx-0 mx-6">
+        <div className="flex flex-col items-center justify-center w-full md:mx-0 mx-0">
           <div>
             <h1 className="text-4xl font-bold">Brand Details</h1>
             <p>Enter your brand details</p>
