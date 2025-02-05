0f9b247b9bd41a93e520ee27fc9979f2ad51ba3e
mriduljainn <mridul@advayu.club>
2025-02-05 23:43:10 +0530

    uncomment middleware.ts file

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,33 +1,33 @@
-// import { NextResponse } from "next/server";
-// import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-// export function middleware(request: NextRequest) {
-//     const accessToken = request.cookies.get("access_token")?.value;
+export function middleware(request: NextRequest) {
+    const accessToken = request.cookies.get("access_token")?.value;
 
-//     if (!accessToken) {
-//         console.log("No access token found. Redirecting to /auth...");
-//         return NextResponse.redirect(new URL("/auth", request.url));
-//     }
+    if (!accessToken) {
+        console.log("No access token found. Redirecting to /auth...");
+        return NextResponse.redirect(new URL("/auth", request.url));
+    }
 
-//     // Example validation logic (optional)
-//     try {
-//         const isValid = true; // Replace with your token validation logic
-//         if (!isValid) throw new Error("Invalid token");
-//     } catch (error) {
-//         console.log("Invalid token. Redirecting to /auth...");
-//         return NextResponse.redirect(new URL("/auth", request.url));
-//     }
+    // Example validation logic (optional)
+    try {
+        const isValid = true; // Replace with your token validation logic
+        if (!isValid) throw new Error("Invalid token");
+    } catch (error) {
+        console.log("Invalid token. Redirecting to /auth...");
+        return NextResponse.redirect(new URL("/auth", request.url));
+    }
 
-//     return NextResponse.next();
-// }
+    return NextResponse.next();
+}
 
-// export const config = {
-//     matcher: [
-//         "/brand",
-//         "/offers",
-//         "/profile",
-//         "/setting",
-//         "/store",
-//         "/((?!auth|_next|favicon.ico|public).*)", // Fallback to secure all other routes
-//     ],
-// };
+export const config = {
+    matcher: [
+        "/brand",
+        "/offers",
+        "/profile",
+        "/setting",
+        "/store",
+        "/((?!auth|_next|favicon.ico|public).*)", // Fallback to secure all other routes
+    ],
+};
