47680ee5913f2542c864a876ac18e1ec696dfbf1
shivamvishwakarm <vishwakarmashivam2003@gmail.com>
2025-09-05 01:32:17 +0530

    change prod url" "

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -31,16 +31,16 @@ export const paymentMethodsOptions = [
 export const URLREGEX =
   /^(https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/g;
 
-// export const LAMBDA_URL =
-//   "https://mgr89ujp5i.execute-api.ap-south-1.amazonaws.com/dashboard";
-export const LAMBDA_URL = "http://localhost:4200";
+export const LAMBDA_URL =
+  "https://mgr89ujp5i.execute-api.ap-south-1.amazonaws.com/dashboard";
+// export const LAMBDA_URL = "http://localhost:4200";
 
 export const AWS_IMAGE_UPLOAD_URL =
   "https://mgr89ujp5i.execute-api.ap-south-1.amazonaws.com/dashboard/upload/image";
 
-// export const AWS_IMAGE_GET_URL =
-//   "https://mgr89ujp5i.execute-api.ap-south-1.amazonaws.com/dashboard/upload/url";
+export const AWS_IMAGE_GET_URL =
+  "https://mgr89ujp5i.execute-api.ap-south-1.amazonaws.com/dashboard/upload/url";
 
-export const AWS_IMAGE_GET_URL = "http://localhost:4200/dashboard/upload/url"
+// export const AWS_IMAGE_GET_URL = "http://localhost:4200/dashboard/upload/url"
 
 export const OUTLET_BUCKET_NAME = "advayu-brands-assets/outlet-assets";
