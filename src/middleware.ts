16dddfcc528c6210f3232f573953ed9140a3648e
shivamvishwakarm <vishwakarmashivam2003@gmail.com>
2025-09-05 12:10:12 +0530

    comment: token check

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,11 @@
 import { NextResponse, type NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  const token = request.cookies.get("access_token")?.value;
-  if (!token) {
-    // No token = Unauthorized
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
+  // const token = request.cookies.get("access_token")?.value;
+  // if (!token) {
+  //   // No token = Unauthorized
+  //   return NextResponse.redirect(new URL("/login", request.url));
+  // }
 
   // try {
   //   // Validate the token
