60d5e9c8595d9d46f5fd20bac53b40e62d363c10
shivamvishwakarm <vishwakarmashivam2003@gmail.com>
2025-09-05 12:46:08 +0530

    add redux provider in dashboard

--- src/components/layout/dashboard.tsx
+++ src/components/layout/dashboard.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect, useRef } from "react";
 import CouponCard, { NoActiveOffers } from "@/components/cards/couponCard";
 import { UserRound } from "lucide-react";
 import GaugeComponent from "@/components/ui/ProgressBar";
-import { useSelector } from "react-redux";
+import { Provider, useSelector } from "react-redux";
 import { useRouter } from "next/navigation";
 import { RootState } from "@/store/store";
 import { useDispatch } from "react-redux";
@@ -13,6 +13,7 @@ import { useLogout } from "@/hooks/use-auth";
 import { setBrandUser } from "@/store/globalSlice/brandUserSlice";
 import { CouponCardSkeleton } from "../ui/skeleton";
 import PaginationButton from "../pagination-button";
+import store from "@/store/store";
 
 const Dashboard = () => {
   // console.log("dashboard: user:", user);
@@ -136,80 +137,82 @@ const Dashboard = () => {
 
   return (
     <>
-      <div className="md:pl-10 md:mt-16 mt-[2rem]">
-        <div className="flex justify-between items-center">
-          <h1 className="md:text-3xl text-xl font-bold">
-            Advayu X {user?.name}
-          </h1>
-          <button
-            onClick={toggleMenu}
-            className=" md:mr-6 bg-gray-100 rounded-full p-2">
-            <UserRound size={32} />
-          </button>
+      <Provider store={store}>
+        <div className="md:pl-10 md:mt-16 mt-[2rem]">
+          <div className="flex justify-between items-center">
+            <h1 className="md:text-3xl text-xl font-bold">
+              Advayu X {user?.name}
+            </h1>
+            <button
+              onClick={toggleMenu}
+              className=" md:mr-6 bg-gray-100 rounded-full p-2">
+              <UserRound size={32} />
+            </button>
+          </div>
+          {isMenuVisible && (
+            <div
+              ref={menuRef}
+              className="absolute md:right-[2rem] right-0  mt-1 w-[8rem] bg-white border rounded-lg shadow-lg z-50  ">
+              <ul className="text-gray-700">
+                <li
+                  className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+                  onClick={() => {
+                    router.push("/profile");
+                  }}>
+                  Profile
+                </li>
+                <li
+                  className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+                  onClick={() => {
+                    console.log("Logout clicked");
+                    logout();
+                    // Handle logout logic here
+                  }}>
+                  Logout
+                </li>
+              </ul>
+            </div>
+          )}
+
+          <div className="flex flex-col mt-8">
+            <h2 className="text-xl font-bold md:block hidden">
+              Active offers ({total || 0})
+            </h2>
+
+            <div className="md:block hidden">
+              <div className="overflow-x-auto">{renderOfferCards()}</div>
+            </div>
+          </div>
         </div>
-        {isMenuVisible && (
-          <div
-            ref={menuRef}
-            className="absolute md:right-[2rem] right-0  mt-1 w-[8rem] bg-white border rounded-lg shadow-lg z-50  ">
-            <ul className="text-gray-700">
-              <li
-                className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
-                onClick={() => {
-                  router.push("/profile");
-                }}>
-                Profile
-              </li>
-              <li
-                className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
-                onClick={() => {
-                  console.log("Logout clicked");
-                  logout();
-                  // Handle logout logic here
-                }}>
-                Logout
-              </li>
-            </ul>
+
+        <div className="md:hidden">
+          <GaugeComponent />
+          <div className="md:ml-[16.3px] px-4">
+            <h2 className="text-xl font-bold mb-4">
+              Active offers ({offers && total})
+            </h2>
+            {isLoading ? (
+              <CouponCardSkeleton />
+            ) : (
+              <div className="overflow-x-auto">{renderOfferCards()}</div>
+            )}
+          </div>
+        </div>
+        <PaginationButton
+          onNext={handleNext}
+          onPrev={handlePrevious}
+          currentPage={page}
+          totalPages={totalPages}
+        />
+        {/* Error Handling */}
+        {error && (
+          <div className="error-message text-red-500">
+            {typeof error === "string"
+              ? error
+              : error.message || error.toString()}
           </div>
         )}
-
-        <div className="flex flex-col mt-8">
-          <h2 className="text-xl font-bold md:block hidden">
-            Active offers ({total || 0})
-          </h2>
-
-          <div className="md:block hidden">
-            <div className="overflow-x-auto">{renderOfferCards()}</div>
-          </div>
-        </div>
-      </div>
-
-      <div className="md:hidden">
-        <GaugeComponent />
-        <div className="md:ml-[16.3px] px-4">
-          <h2 className="text-xl font-bold mb-4">
-            Active offers ({offers && total})
-          </h2>
-          {isLoading ? (
-            <CouponCardSkeleton />
-          ) : (
-            <div className="overflow-x-auto">{renderOfferCards()}</div>
-          )}
-        </div>
-      </div>
-      <PaginationButton
-        onNext={handleNext}
-        onPrev={handlePrevious}
-        currentPage={page}
-        totalPages={totalPages}
-      />
-      {/* Error Handling */}
-      {error && (
-        <div className="error-message text-red-500">
-          {typeof error === "string"
-            ? error
-            : error.message || error.toString()}
-        </div>
-      )}
+      </Provider>
     </>
   );
 };
